a6b7fe99821bb4007b392b67513c617ea8a2e639
AliBarkinKara07 <111884407+AliBarkinKara07@users.noreply.github.com>
2022-10-27 16:41:40 +0300

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak Ayındasınız");
+                    break;
+                case 2:
+                    Console.WriteLine("Ocak Ayındasınız");
+                    break;
+                case 3:
+                    Console.WriteLine("Ocak Ayındasınız");
+                    break;
+                case 10:
+                    Console.WriteLine("Ekim Ayındasınız");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış Veri Girdiniz");
+                    break;
+            }
+        }
+    }
+}
